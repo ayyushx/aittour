746e99c3e3d14e8c2e033bfa2686bf472191a67f
Ayush Badoni <ayushbadoni@Ayushs-Laptop.local>
2024-08-26 09:08:30 +0530

    orientation version added

--- js/main.js
+++ js/main.js
@@ -8,14 +8,16 @@ const panorama7 = new PANOLENS.ImagePanorama('images/towardsoac.jpeg');
 const panorama8 = new PANOLENS.ImagePanorama('images/xerox.jpeg');
 const panorama9 = new PANOLENS.ImagePanorama('images/oactree.jpeg');
 const panorama10 = new PANOLENS.ImagePanorama('images/oac.jpeg');
+const panorama11 = new PANOLENS.ImagePanorama('images/trial.jpeg');
 let imageContainer = document.querySelector('.image-container');
 
 
 var infospotPositions = [
-    new THREE.Vector3(-2136.06, 16.30, 890.14),
-    new THREE.Vector3(-3136.06, 296.30, -4290.14),  
+    new THREE.Vector3(2500.386,50.669,200),
+    new THREE.Vector3(1681.414, -4000.661, -985.730),  
     new THREE.Vector3(-2136, 16, 890), 
     new THREE.Vector3(-1500, 30, 890), //parking right
+    new THREE.Vector3(1676.9529786428825, 5800.410002336587, 2032.907649701389),
   ];
 
 const viewer = new PANOLENS.Viewer({
@@ -40,5 +42,24 @@ panorama8.link(panorama9 , infospotPositions[0]);//xerox to oactree
 panorama9.link( panorama8, infospotPositions[1]);//oactree to xerox
 panorama9.link( panorama10 ,  infospotPositions[0]);//oactree to oac
 panorama10.link(panorama9,infospotPositions[1]);
-viewer.add( panorama,panorama2,panorama3,panorama4,panorama5,panorama6 ,panorama7,panorama8,panorama9,panorama10);
+
+//hello
+
+panorama.link(panorama11 , infospotPositions[1] );
+panorama11.link(panorama,infospotPositions[4]);
+viewer.add( panorama,panorama2,panorama3,panorama4,panorama5,panorama6 ,panorama7,panorama8,panorama9,panorama10,panorama11);
+
+//To enable the rotation according to the orientation
+window.addEventListener('deviceorientation', (event) => {
+  const alpha = event.alpha ? THREE.Math.degToRad(event.alpha) : 0;
+  const beta = event.beta ? THREE.Math.degToRad(event.beta) : 0;
+  const gamma = event.gamma ? THREE.Math.degToRad(event.gamma) : 0;
+
+  // Calculate the rotation quaternion
+  const quaternion = new THREE.Quaternion();
+  quaternion.setFromEuler(new THREE.Euler(beta, alpha, -gamma, 'YXZ'));
+
+  // Apply the rotation to the panorama
+  panorama.rotation.setFromQuaternion(quaternion);
+}, true);
 
